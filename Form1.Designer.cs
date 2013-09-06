a78f3392d50c4a5c550a6af0ebbcac48f3d001b0
Thomas Fritchman <tcfritchman@gmail.com>
2013-09-05 21:46:28 -0700

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace canyouhearmenow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playButton = new System.Windows.Forms.Button();
+            this.recordButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.RewindButton = new System.Windows.Forms.Button();
+            this.fastForwardButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.endButton = new System.Windows.Forms.Button();
+            this.labelCurrentTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(520, 89);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // recordButton
+            // 
+            this.recordButton.Location = new System.Drawing.Point(520, 147);
+            this.recordButton.Name = "recordButton";
+            this.recordButton.Size = new System.Drawing.Size(75, 23);
+            this.recordButton.TabIndex = 1;
+            this.recordButton.Text = "Record";
+            this.recordButton.UseVisualStyleBackColor = true;
+            this.recordButton.Click += new System.EventHandler(this.recordButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(520, 118);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // RewindButton
+            // 
+            this.RewindButton.Location = new System.Drawing.Point(520, 176);
+            this.RewindButton.Name = "RewindButton";
+            this.RewindButton.Size = new System.Drawing.Size(75, 23);
+            this.RewindButton.TabIndex = 3;
+            this.RewindButton.Text = "Rew";
+            this.RewindButton.UseVisualStyleBackColor = true;
+            // 
+            // fastForwardButton
+            // 
+            this.fastForwardButton.Location = new System.Drawing.Point(520, 205);
+            this.fastForwardButton.Name = "fastForwardButton";
+            this.fastForwardButton.Size = new System.Drawing.Size(75, 23);
+            this.fastForwardButton.TabIndex = 4;
+            this.fastForwardButton.Text = "FF";
+            this.fastForwardButton.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(520, 234);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // endButton
+            // 
+            this.endButton.Location = new System.Drawing.Point(520, 263);
+            this.endButton.Name = "endButton";
+            this.endButton.Size = new System.Drawing.Size(75, 23);
+            this.endButton.TabIndex = 6;
+            this.endButton.Text = "End";
+            this.endButton.UseVisualStyleBackColor = true;
+            // 
+            // labelCurrentTime
+            // 
+            this.labelCurrentTime.AutoSize = true;
+            this.labelCurrentTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentTime.Location = new System.Drawing.Point(514, 43);
+            this.labelCurrentTime.Name = "labelCurrentTime";
+            this.labelCurrentTime.Size = new System.Drawing.Size(65, 31);
+            this.labelCurrentTime.TabIndex = 7;
+            this.labelCurrentTime.Text = "time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(640, 376);
+            this.Controls.Add(this.labelCurrentTime);
+            this.Controls.Add(this.endButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.fastForwardButton);
+            this.Controls.Add(this.RewindButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.recordButton);
+            this.Controls.Add(this.playButton);
+            this.Name = "Form1";
+            this.Text = "canyouhearmenow?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button recordButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button RewindButton;
+        private System.Windows.Forms.Button fastForwardButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button endButton;
+        private System.Windows.Forms.Label labelCurrentTime;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+
+namespace canyouhearmenow
+{
+    public partial class Form1 : Form
+    {
+        // recording
+        private IWaveIn waveIn;
+        private WaveFileWriter writer;
+        private string outputFilename;
+        private readonly string outputFolder;
+
+        // playback
+        private IWavePlayer wavePlayer;
+        private AudioFileReader file;
+        private string fileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            outputFolder = Path.Combine(Path.GetTempPath(), "NAudioDemo");
+            Directory.CreateDirectory(outputFolder);
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new EventHandler(timer1_Tick);
+        }
+
+        private static string FormatTimeSpan(TimeSpan ts)
+        {
+            return string.Format("{0:D2}:{1:D2}", (int)ts.TotalMinutes, ts.Seconds);
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            if (file != null)
+            {
+                labelCurrentTime.Text = FormatTimeSpan(file.CurrentTime);
+            }
+        }
+
+        void SimplePlaybackPanel_Disposed(object sender, EventArgs e)
+        {
+            CleanUp();
+        }
+
+        
+
+
+
+        private void recordButton_Click(object sender, EventArgs e)
+        {
+            if (waveIn == null)
+            {
+                outputFilename = String.Format("NAudioDemo {0:yyy-MM-dd HH-mm-ss}.wav", DateTime.Now);
+                waveIn = new WaveIn();
+                waveIn.WaveFormat = new WaveFormat(44100, 1);
+                writer = new WaveFileWriter(Path.Combine(outputFolder, outputFilename), waveIn.WaveFormat);
+
+                waveIn.DataAvailable += OnDataAvailable;
+                waveIn.RecordingStopped += OnRecordingStopped;
+                waveIn.StartRecording();
+                recordButton.Enabled = false;
+            }
+        }
+
+        private void BeginPlayback(string filename)
+        {
+            Debug.Assert(this.wavePlayer == null);
+            this.wavePlayer = CreateWavePlayer();
+            this.file = new AudioFileReader(Path.Combine(outputFolder, filename));
+            this.file.Volume = 1.0F;
+            Debug.Print(Path.Combine(outputFolder, filename));
+            this.wavePlayer.Init(file);
+            this.wavePlayer.PlaybackStopped += wavePlayer_PlaybackStopped;
+            this.wavePlayer.Play();
+            timer1.Enabled = true; // for updating current time
+        }
+
+        private IWavePlayer CreateWavePlayer()
+        {
+            // WaveOut window callbacks
+            return new WaveOutEvent();
+        }
+
+        void wavePlayer_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            // we want to be always on the GUI thread and be able to change GUI components
+            Debug.Assert(!this.InvokeRequired, "PlaybackStopped on wrong thread");
+            // we want it to be safe to clean up input stream and playback device in the handler for PlaybackStopped
+            CleanUp();
+            //EnableButtons(false);
+            timer1.Enabled = false;
+            labelCurrentTime.Text = "00:00";
+            if (e.Exception != null)
+            {
+                MessageBox.Show(String.Format("Playback Stopped due to an error {0}", e.Exception.Message));
+            }
+        }
+
+        void OnRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new EventHandler<StoppedEventArgs>(OnRecordingStopped), sender, e);
+            }
+            else
+            {
+                Cleanup();
+                recordButton.Enabled = true;
+                if (e.Exception != null)
+                {
+                    MessageBox.Show(String.Format("A problem was encountered during recording {0}",
+                                                  e.Exception.Message));
+                }
+            }
+        }
+
+        private void Cleanup()
+        {
+            if (waveIn != null) // working around problem with double raising of RecordingStopped
+            {
+                waveIn.Dispose();
+                waveIn = null;
+            }
+            if (writer != null)
+            {
+                writer.Close();
+                writer = null;
+            }
+        }
+
+        private void CleanUp()
+        {
+            if (this.file != null)
+            {
+                this.file.Dispose();
+                this.file = null;
+            }
+            if (this.wavePlayer != null)
+            {
+                this.wavePlayer.Dispose();
+                this.wavePlayer = null;
+            }
+        }
+
+
+        void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (this.InvokeRequired)
+            {
+                //Debug.WriteLine("Data Available");
+                this.BeginInvoke(new EventHandler<WaveInEventArgs>(OnDataAvailable), sender, e);
+            }
+            else
+            {
+                //Debug.WriteLine("Flushing Data Available");
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+                int secondsRecorded = (int)(writer.Length / writer.WaveFormat.AverageBytesPerSecond);
+            
+            }
+        }
+
+        void StopRecording()
+        {
+            Debug.WriteLine("StopRecording");
+            waveIn.StopRecording();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            if (waveIn != null)
+            {
+                StopRecording();
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            if (fileName == null) fileName = outputFilename;
+            if (fileName != null)
+            {
+                BeginPlayback(fileName);
+            }
+        }
+
+    }
+}
